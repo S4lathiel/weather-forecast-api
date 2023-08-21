1c200be0d83c8ab4ab2f522a0afb436b9e8587b5
WSalathiel <salathieldsf@gmail.com>
2023-08-20 22:10:33 -0300

    v1.0.01

--- README.md
+++ README.md
@@ -1,8 +1,66 @@
-Olá, para isto funcionar, É necessário ter Node.js Instalado na máquina, depois no seu terminal digite:
+Claro, peço desculpas pela omissão. Aqui está o README em inglês, incluindo as instruções para instalar a biblioteca `weather-js`:
 
-npm i cors
-npm i express
-npm i weather-js
+```markdown
+# Weather Forecast App
 
-E depois
+This is a simple app that utilizes the weather-js API to provide weather forecast information for a given city and state.
+
+## Prerequisites
+
+- Node.js installed
+- Modern web browser
+
+## How to Use
+
+1. Clone this repository to your computer:
+
+```bash
+git clone https://github.com/S4lathiel/previsaodoTempo.git
+```
+
+2. Navigate to the project directory:
+
+```bash
+cd weather-forecast-app
+```
+
+3. Install the dependencies:
+
+```bash
+npm install
+```
+
+4. Install the weather-js library:
+
+```bash
+npm install weather-js
+```
+
+5. Start the API server:
+
+```bash
 node app.js
+```
+
+6. Open your web browser and access the following URL:
+
+```
+http://localhost:3000
+```
+
+This will open the weather forecast app interface.
+
+## Customization
+
+- The user interface is in the `previsao.html` file.
+- The CSS style is in the `css` directory.
+- The JavaScript for the user interface is in the `js` directory.
+- The API server is defined in the `app.js` file.
+
+## Contribution
+
+If you want to contribute to this project, feel free to open an issue or submit a pull request.
+---
+
+Created by [S4lathiel](https://github.com/S4lathiel)
+```
--- js/index.js
+++ js/index.js
@@ -1,38 +1,73 @@
-// Variáveis que representam os elementos HTML
 var btnConsultar = document.querySelector("#consultar"),
-cityTxt = document.querySelector("#cityTxt"),
-weatherInfo = document.querySelector("#weather-info"),
-ventania = document.querySelector("#ventania"),
-humidade = document.querySelector("#humidade"),
-tempMin = document.querySelector("#tempMin"),
-tempMax = document.querySelector("#tempMax"),
-proximoDia = document.querySelector("#proximoDia"),
-proximoDia2 = document.querySelector("#proximoDia2");
+    cityTxt = document.querySelector("#cityTxt"),
+    weatherContainer = document.querySelector(".weather-container");
 
-// Adiciona um evento ao botão "consultar" para chamar a função "consultar"
 btnConsultar.addEventListener("click", function (event) {
     event.preventDefault();
     consultar();
 });
 
-// Função que faz uma requisição para a API com a cidade e estado informados
-function consultar() {
-    // Obtém a cidade e estado informados pelo usuário
-    var cidadeEstado = cityTxt.value;
+function criarCartao(titulo, valor, clima) {
+    var emoji = getEmoji(titulo, valor);
+    var card = `
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title weather-title">${emoji} ${titulo}</h5>
+            <p class="card-text weather-detail">${valor}</p>
+          </div>
+        </div>
+      </div>
+    `;
+    weatherContainer.innerHTML += card;
+  }
+  
+  function getEmoji(titulo, valor) {
+    if (titulo.includes('Temp. Atual')) return '🌡️';
+    if (titulo.includes('Ventania')) return '💨';
+    if (titulo.includes('Humidade')) {
+      var humidity = parseFloat(valor.split(' ')[1]);
+      if (humidity < 30) return '🌵';
+      if (humidity >= 30 && humidity < 60) return '🌦️';
+      return '💧';
+    }
+    if (titulo.includes('Temp. Mínima')) return '❄️';
+    if (titulo.includes('Temp. Máxima')) return '🔥';
+    if (titulo.includes('Amanhã')) return '📅';
+    if (titulo.includes('Depois de amanhã')) return '📆';
+    return '';
+  }
+  
 
-    // Faz uma requisição para a API, espera a resposta, a transforma em JSON
-    // e, em seguida, preenche os elementos HTML com as informações da previsão do tempo
+function getWeatherIcon(condition) {
+    condition = condition.toLowerCase();
+    if (condition.includes('rain')) return 'fas fa-cloud-showers-heavy';
+    if (condition.includes('snow')) return 'fas fa-snowflake';
+    if (condition.includes('sunny') || condition.includes('clear')) return 'fas fa-sun';
+    if (condition.includes('cloud')) return 'fas fa-cloud';
+    if (condition.includes('wind')) return 'fas fa-wind';
+    if (condition.includes('fog')) return 'fas fa-smog';
+    if (condition.includes('haze')) return 'fas fa-smog';
+    return 'fas fa-cloud'; // Ícone padrão para outras condições
+}
+
+function consultar() {
+    var cidadeEstado = cityTxt.value;
+    weatherContainer.innerHTML = '';
+
     fetch(`http://localhost:3000/${cidadeEstado}`)
         .then(response => response.json())
         .then(resultado => {
-            weatherInfo.innerHTML = `Temp. Atual: ${resultado[0]["current"]["temperature"]} C°`
-            ventania.innerHTML = `Ventania: ${resultado[0]["current"]["winddisplay"].split(" ")[0]} km/h`
-            humidade.innerHTML = `Humidade: ${resultado[0]["current"]["humidity"]} %`
-            tempMin.innerHTML = `Temp. Mínima: ${resultado[0]["forecast"][0]["low"]} C°`
-            tempMax.innerHTML = `Temp. Máxima: ${resultado[0]["forecast"][0]["high"]} C°`
-            proximoDia.innerHTML = `Amanhã:<br>Temp. Mínima: ${resultado[0]["forecast"][1]["low"]} C°<br>Temp Máxima: ${resultado[0]["forecast"][1]["high"]} C°`
-            proximoDia2.innerHTML = `Depois de amanhã:<br>Temp. Mínima: ${resultado[0]["forecast"][2]["low"]} C°<br>Temp Máxima: ${resultado[0]["forecast"][2]["high"]} C°`
-            })
+            var climaAtual = resultado[0]["current"]["skytext"];
+
+            criarCartao('Temp. Atual', `${resultado[0]["current"]["temperature"]} C°`, climaAtual);
+            criarCartao('Ventania', `${resultado[0]["current"]["winddisplay"].split(" ")[0]} km/h`, climaAtual);
+            criarCartao('Humidade', `${resultado[0]["current"]["humidity"]} %`, climaAtual);
+            criarCartao('Temp. Mínima', `${resultado[0]["forecast"][0]["low"]} C°`, climaAtual);
+            criarCartao('Temp. Máxima', `${resultado[0]["forecast"][0]["high"]} C°`, climaAtual);
+            criarCartao('Amanhã', `Mínima: ${resultado[0]["forecast"][1]["low"]} C°\nMáxima: ${resultado[0]["forecast"][1]["high"]} C°`, climaAtual);
+            criarCartao('Depois de amanhã', `Mínima: ${resultado[0]["forecast"][2]["low"]} C°\nMáxima: ${resultado[0]["forecast"][2]["high"]} C°`, climaAtual);
+        })
         .catch(error => {
             console.error(error);
         });
